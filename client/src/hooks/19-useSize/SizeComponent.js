f298772eb9bb42040f7c45ffda1f2540236d7c8e
Tyler Huyser <tyler.huyser@gmail.com>
2022-01-31 23:04:16 -0800

    created useSize component and hook.

---
+++ client/src/hooks/19-useSize/SizeComponent.js
@@ -0,0 +1,14 @@
+import { useRef } from "React"
+import useSize from "./useSize"
+
+export default function SizeComponent() {
+  const ref = useRef()
+  const size = useSize(ref)
+
+  return (
+    <>
+      <div>{JSON.stringify(size)}</div>
+      <textarea ref={ref}></textarea>
+    </>
+  )
+}
